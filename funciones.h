2479d87a5fab441dbac0b36258f108c7689c01fe
Hertzfeld <62438233+Hertzfeld@users.noreply.github.com>
2020-04-09 13:50:59 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,8 +1,9 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
 
-int factorial(int);//calcula el factorial de un num sin usar recursividad
+unsigned long long int factorial(int);//calcula el factorial de un num sin usar recursividad
 int combinatoria(int, int);
+void fibonacci(int);//muestra la serie de fibonacci
 void perteneceFibonacci(int);
 void esNatural(int);
 #endif // FUNCIONES_H_INCLUDED
